96e99b0092f6dd2bd60bdffaf55e7066ef6df606
Andrey Malin <andreu6454@gmail.com>
2023-07-12 00:55:42 +0300

    add config

---
+++ config/build/types/config.ts
@@ -0,0 +1,18 @@
+
+export type BuiltMode = 'production' | 'development'
+
+export interface BuildEnv {
+    mode: BuiltMode,
+    port: number
+}
+export interface BuildPaths {
+    entry: string,
+    build: string,
+    html: string
+}
+export interface BuildOptions {
+    mode: BuiltMode;
+    paths: BuildPaths;
+    isDev: boolean,
+    port: number
+}
