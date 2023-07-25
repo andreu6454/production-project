2f97dc8ed1fa04aee64ed8d15fec47c31a468f83
Andrey Malin <andreu6454@gmail.com>
2023-07-26 01:20:26 +0300

    -upgrade sidebar -add buttons themes

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import {Theme, useTheme} from "app/providers/ThemeProvider";
 import LightIcon from 'shared/assets/icons/theme-light.svg'
 import DarkIcon from 'shared/assets/icons/theme-dark.svg'
-import {Button} from "shared/ui/Button/Button";
+import {Button, ButtonTheme} from "shared/ui/Button/Button";
 
 
 interface ThemeSwitcherProps {
@@ -19,6 +19,7 @@ export const ThemeSwitcher = ({className}: ThemeSwitcherProps) => {
         <Button
             onClick={toggleTheme}
             className={classNames(cls.ThemeSwitcher, {}, [className])}
+            theme={ButtonTheme.CLEAR}
         >
             {theme === Theme.DARK ? <DarkIcon/> : <LightIcon/>}
         </Button>
