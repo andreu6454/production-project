fd94a4d385a45ea2fddcb63a202d4b55bb6b33aa
Andrey Malin <andreu6454@gmail.com>
2023-07-31 16:38:05 +0300

    - фикс темы для модальных окон

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -2,6 +2,7 @@ import {classNames} from "shared/lib/classNames/classNames";
 import cls from './Modal.module.scss'
 import React, {ReactNode, useCallback, useEffect, useRef, useState} from "react";
 import {Portal} from "shared/ui/Portal/Portal";
+import {useTheme} from "app/providers/ThemeProvider";
 
 interface ModalProps {
     className?: string;
@@ -19,6 +20,8 @@ export const Modal = (props: ModalProps) => {
         isOpen,
         onClose,
     } = props
+
+    const {theme} = useTheme()
 
     const [isClosing, setIsClosing] = useState(false)
     const timeRef = useRef<ReturnType<typeof setTimeout>>()
@@ -61,7 +64,7 @@ export const Modal = (props: ModalProps) => {
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={classNames(cls.Modal, mods, [className, theme])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div className={cls.content} onClick={onContentClick}>
                         {children}
