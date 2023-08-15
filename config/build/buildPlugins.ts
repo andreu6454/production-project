2ab8cbc95798466e588baad7c86ef6100b11dbf6
Andrey Malin <andreu6454@gmail.com>
2023-08-15 20:40:49 +0300

    production

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer'
+import CopyPlugin from "copy-webpack-plugin";
 
 export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): webpack.WebpackPluginInstance[] {
 
@@ -22,6 +23,11 @@ export function buildPlugins({paths, isDev, apiUrl, project}: BuildOptions): web
             __API__: JSON.stringify(apiUrl),
             __PROJECT__: JSON.stringify(project)
         }),
+        new CopyPlugin({
+            patterns: [
+                {from: paths.locales, to: paths.buildLocales},
+            ]
+        })
     ]
 
     if (isDev) {
