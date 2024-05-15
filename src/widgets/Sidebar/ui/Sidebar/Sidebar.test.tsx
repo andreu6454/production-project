0aa6acc66842a50cc2698ea1ea4001997bbb2c85
Andrey Malin <andreu6454@gmail.com>
2024-05-15 20:18:09 +0300

    - фикс стилей для теста

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -12,6 +12,6 @@ describe('Sidebar tests', () => {
         const toggleBtn = screen.getByTestId('sidebar-toggle')
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn)
-        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsedRedesigned')
     })
 })
